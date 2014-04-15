8988db7c1c295fd69e3b8842c5cb7885f3f5bc57
syslight <d.pei@me.com>
2014-04-15 23:55:10 +0800

    init
    init commit

---
+++ Demo2/app/src/main/java/com/syslight/demo2/app/ui/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.syslight.demo2.app.ui;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import com.syslight.demo2.app.R;
+
+public class LoginActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
